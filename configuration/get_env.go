fa1b3e29aab7ec64a612ddfb72df64ac8d474bad
anca <rrrusdiansyah@gmail.com>
2023-04-12 15:52:37 +0700

    setting env1

--- configuration/get_env.go
+++ configuration/get_env.go
@@ -1,18 +1,14 @@
 package configuration
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func Get_env(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Println("Error Loading .env")
-		panic(err)
-	}
+	godotenv.Load(".env")
+
 	return os.Getenv(key)
 }
 
