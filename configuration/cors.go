da8e3f84617a1d0acda0dee5e1877ca268578aa6
anca <rrrusdiansyah@gmail.com>
2023-04-13 12:13:15 +0700

    setting cors 1

--- configuration/cors.go
+++ configuration/cors.go
@@ -1,6 +1,6 @@
 package configuration
 
 func AllowOrigins() []string {
-	url := []string{"http://127.0.0.1:3001,", "https://projectprakerja2023-production.up.railway.app/"}
+	url := []string{"*"}
 	return url
 }
